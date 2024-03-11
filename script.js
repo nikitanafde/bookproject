0b95f1953964a32af6b6f43f514c7005adc75800
Sa51 <sa51@Skills-Academy-53.local>
2024-03-11 18:15:05 -0400

    Made some changes to html, java, and css

---
+++ script.js
@@ -0,0 +1,25 @@
+//  let age = 25; 
+// const birthYear = 1995;
+// console.log('Age:', age);
+// let sum = age -10;
+// console.log('Age 10 years ago:', sum);
+// if(age >=18) {
+//   console.log('You are an adult.');
+// } else {
+//   console.log('You are a minor.');
+// }
+// if(age >=18 && age <= 30){
+//   console.log('You are in your twenties or late teens')
+// }
+// if (age < 18 || age > 30) {
+//   console.log('You are either younger than 18 or older than 30.');
+// }
+// let isMinor = age <18;
+// if (!isMinor) {
+//   console.log('You are not a minor');
+// }
+let genre1 = False; 
+let genre2 = False; 
+let genre3 = False; 
+let genre4 = False; 
+let genre5 = False; 
